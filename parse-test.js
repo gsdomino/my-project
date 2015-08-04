6a7db8e84e8f73dddd776553b165712714ec1cba
gsdomino <georgedomino@GSD-MacBook-Pro.local>
2015-08-03 21:45:12 -0400

    add parse-test.js

---
+++ parse-test.js
@@ -0,0 +1,13 @@
+$(function() {
+ 
+    Parse.$ = jQuery;
+    // Replace this line with the one on your Quickstart Guide Page
+    Parse.initialize("t7j3bcOoZU9elelY9a9kZP5iOwwyNMfTc58pc3kc", "DMxFjUwtS1jvjTdb3yUFOUjiC1AUTeg8ohmr0G6m");
+ 
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    testObject.save({foo: "bar"}).then(function(object) {
+      alert("yay! it worked");
+    });
+ 
+});
